b3190abc9d1cd149607445b8d22b6fcd3793e92b
sunHuoHeng <46321369+sunHuoHeng@users.noreply.github.com>
2020-09-06 17:44:04 +0800

    增加底部导航条

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import app from './App.vue';
 import { Header } from 'mint-ui';
 import 'mint-ui/lib/style.css'
 import './lib/mui/css/mui.min.css'
-import ''
 Vue.component(Header.name, Header);
 new Vue({
     el: '#app',
